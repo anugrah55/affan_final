44970021d9ca32cc336e5c559d74d2164388e006
Anugrah Sengar <anugrahsengar92@Gmail.com>
2026-04-21 00:05:51 +0530

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/focus_architect/",
+  base: "/affan_final/",
   plugins: [react(), tailwindcss()],
 })
